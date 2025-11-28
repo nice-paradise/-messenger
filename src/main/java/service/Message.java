23eda71eccf593a246f5f3ff4416c72e4526b2ef
albert101-afk <agarifullin09@gmail.com>
2025-11-28 23:29:54 +0300

    Переделана менюшка

--- src/main/java/service/Message.java
+++ src/main/java/service/Message.java
@@ -2,20 +2,26 @@ package service;
 
 import user.User;
 import user.Auth;
-import user.UserToUserMessage;
+import user.UserChat;
+import group.Group;
+import group.GroupManager;
 import java.util.Scanner;
 
 public class Menu {
+    private static Auth auth;
 
-    public static void showMenu(User currentUser, Auth auth, DataBase dataBase) {
+    public static void showMenu(User currentUser, Auth authInstance, DataBase dataBase) {
+        auth = authInstance;
         Scanner scanner = new Scanner(System.in);
         boolean exit = false;
 
         while (!exit) {
-            System.out.println("\n=== МЕНЮ ===");
-            System.out.println("1. Написать сообщение");
+            System.out.println("\n=== ГЛАВНОЕ МЕНЮ ===");
+            System.out.println("Добро пожаловать, " + currentUser.getName() + "!");
+            System.out.println("1. Написать личное сообщение");
             System.out.println("2. Посмотреть мои сообщения");
-            System.out.println("3. Выйти");
+            System.out.println("3. Группы");
+            System.out.println("0. Выйти");
             System.out.print("Выберите: ");
 
             int choice = scanner.nextInt();
@@ -23,14 +29,17 @@ public class Menu {
 
             switch (choice) {
                 case 1:
-                    sendMessage(currentUser, auth, dataBase);
+                    sendMessage(currentUser, dataBase, scanner);
                     break;
                 case 2:
                     dataBase.showUserMessages(currentUser);
                     break;
                 case 3:
+                    showGroupMenu(currentUser, dataBase, scanner);
+                    break;
+                case 0:
                     exit = true;
-                    System.out.println("Выход...");
+                    System.out.println("До свидания!");
                     break;
                 default:
                     System.out.println("Неверный выбор!");
@@ -38,12 +47,116 @@ public class Menu {
         }
     }
 
-    private static void sendMessage(User currentUser, Auth auth, DataBase dataBase) {
-        Scanner scanner = new Scanner(System.in);
+    private static void showGroupMenu(User currentUser, DataBase dataBase, Scanner scanner) {
+        GroupManager groupManager = new GroupManager();
+        boolean back = false;
 
-        System.out.println("\n=== НАПИСАТЬ СООБЩЕНИЕ ===");
+        while (!back) {
+            System.out.println("\n=== МЕНЮ ГРУПП ===");
+            System.out.println("1. Создать группу");
+            System.out.println("2. Добавить пользователя в группу");
+            System.out.println("3. Написать в группу");
+            System.out.println("4. Просмотреть группу");
+            System.out.println("5. Мои группы");
+            System.out.println("6. Все группы");
+            System.out.println("0. Назад в главное меню");
+            System.out.print("Выберите: ");
+
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (choice) {
+                case 1:
+                    createGroup(currentUser, groupManager, scanner);
+                    break;
+                case 2:
+                    addUserToGroup(groupManager, scanner);
+                    break;
+                case 3:
+                    writeToGroup(currentUser, groupManager, scanner);
+                    break;
+                case 4:
+                    viewGroup(groupManager, scanner);
+                    break;
+                case 5:
+                    groupManager.showUserGroups(currentUser);
+                    break;
+                case 6:
+                    groupManager.showAllGroups();
+                    break;
+                case 0:
+                    back = true;
+                    break;
+                default:
+                    System.out.println("Неверный выбор!");
+            }
+        }
+    }
+
+    private static void createGroup(User currentUser, GroupManager groupManager, Scanner scanner) {
+        System.out.print("Введите название группы: ");
+        String name = scanner.nextLine();
+        groupManager.createGroup(name, currentUser);
+    }
+
+    private static void addUserToGroup(GroupManager groupManager, Scanner scanner) {
+        groupManager.showAllGroups();
+        System.out.print("Введите название группы: ");
+        String groupName = scanner.nextLine();
+
+        Group group = groupManager.findGroup(groupName);
+        if (group != null) {
+            auth.showAllUsers();
+            System.out.print("Введите логин пользователя для добавления: ");
+            String userLogin = scanner.nextLine();
+
+            User userToAdd = auth.getUserByLogin(userLogin);
+            if (userToAdd != null) {
+                group.addUser(userToAdd);
+            } else {
+                System.out.println("Пользователь не найден!");
+            }
+        } else {
+            System.out.println("Группа не найдена!");
+        }
+    }
+
+    private static void writeToGroup(User currentUser, GroupManager groupManager, Scanner scanner) {
+        groupManager.showAllGroups();
+        System.out.print("Введите название группы: ");
+        String groupName = scanner.nextLine();
+
+        Group group = groupManager.findGroup(groupName);
+        if (group != null) {
+            System.out.print("Введите сообщение для группы: ");
+            String message = scanner.nextLine();
+
+            String formattedMessage = currentUser.getName() + ": " + message;
+            group.addChatMessage(formattedMessage);
+            System.out.println("Сообщение отправлено в группу '" + groupName + "'");
+        } else {
+            System.out.println("Группа не найдена!");
+        }
+    }
+
+    private static void viewGroup(GroupManager groupManager, Scanner scanner) {
+        groupManager.showAllGroups();
+        System.out.print("Введите название группы: ");
+        String groupName = scanner.nextLine();
+
+        Group group = groupManager.findGroup(groupName);
+        if (group != null) {
+            System.out.println("\n=== ИНФОРМАЦИЯ О ГРУППЕ '" + groupName + "' ===");
+            group.showAllMembers();
+            group.showChatHistory();
+        } else {
+            System.out.println("Группа не найдена!");
+        }
+    }
+
+    private static void sendMessage(User currentUser, DataBase dataBase, Scanner scanner) {
+        System.out.println("\n=== НАПИСАТЬ ЛИЧНОЕ СООБЩЕНИЕ ===");
         auth.showAllUsers();
-
         System.out.print("Введите логин получателя: ");
         String login = scanner.nextLine();
 
@@ -51,10 +164,8 @@ public class Menu {
         if (recipient != null && !login.equals(currentUser.getLogin())) {
             System.out.print("Введите сообщение: ");
             String message = scanner.nextLine();
-
-            UserToUserMessage userMessage = new UserToUserMessage(currentUser, recipient, message);
+            UserChat userMessage = new UserChat(currentUser, recipient, message);
             dataBase.addMessage(userMessage);
-
             System.out.println("Сообщение отправлено!");
         } else if (login.equals(currentUser.getLogin())) {
             System.out.println("Нельзя отправить себе!");
