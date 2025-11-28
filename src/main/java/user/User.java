fe042194ba97183650eb67df479f330ec6a49513
askar <askareliseev012007@mail.ru>
2025-11-28 19:42:33 +0300

    создан класс Auth, User, UserChat и в них были расписаны методы

--- src/main/java/user/User.java
+++ src/main/java/user/User.java
@@ -1,4 +1,48 @@
 package user;
 
 public class User {
+    private String login;
+    private String password;
+    private String name;
+    private String id;
+
+
+
+    public User(String login, String password, String id, String name){
+        this.login = login;
+        this.password = password;
+        this.name = name;
+        this.id = id;
+
+
+
+    }
+    public void changePassword(String newPassword){
+        this.password = newPassword;
+
+
+    }
+
+
+    public String getLogin() {
+        return login;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getId(){
+        return id;
+    }
+
+
+
+    public String toString() {
+        return "логин: " + login + ", пароль: " + password + ", id: " + id + ", имя" + name;
+
+
+    }
+
 }
