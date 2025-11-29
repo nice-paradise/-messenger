09d7ed9b091b31e79c7f1afbd50ddbff56f98909
NastraX <askareliseev012007@mail.ru>
2025-11-29 10:29:00 +0300

    удалены неиспользуемы методы

--- src/main/java/user/Auth.java
+++ src/main/java/user/Auth.java
@@ -10,10 +10,11 @@ public class Auth {
         dataBase = new User[100];
         dataBase[0] = new User("Askar11", "1234", "123456", "askar");
         dataBase[1] = new User("Ilnurka", "1234", "1234789", "ilnur");
-        dataBase[2] = new User("Alba","1234","13141","albert");
-        dataBase[3] = new User("Tema","1243","653","artem");
+        dataBase[2] = new User("Temka","1234","13414","artem");
+        dataBase[3] = new User("Alba","1234","11414","albert");
 
         countUsers = 4;
+    }
 
     public User signIn() {
         Scanner scanner = new Scanner(System.in);
@@ -49,6 +50,7 @@ public class Auth {
         return null;
     }
 
+    // ИСПРАВЛЕННЫЙ МЕТОД - не показываем пароли и ID
     public void showAllUsers(){
         System.out.println("\n=== ВСЕ ПОЛЬЗОВАТЕЛИ ===");
         for(int i = 0; i < countUsers; i++){
@@ -58,24 +60,7 @@ public class Auth {
         System.out.println("Всего пользователей: " + countUsers);
     }
 
-    public void showAllUsersSimple(){
-        System.out.println("\n=== ПОЛЬЗОВАТЕЛИ ===");
-        for(int i = 0; i < countUsers; i++){
-            System.out.println((i+1) + ". " + dataBase[i].getName());
-        }
-    }
 
-
-    public void showAllUsersFormatted(){
-        System.out.println("\n┌─────────────────────────────┐");
-        System.out.println("│        СПИСОК УЧАСТНИКОВ     │");
-        System.out.println("├─────────────────────────────┤");
-        for(int i = 0; i < countUsers; i++){
-            System.out.printf("│ %-2d. %-15s (%-10s) │\n",
-                    (i+1), dataBase[i].getName(), dataBase[i].getLogin());
-        }
-        System.out.println("└─────────────────────────────┘");
-    }
 
     public User getUserByLogin(String login) {
         for (int i = 0; i < countUsers; i++) {
